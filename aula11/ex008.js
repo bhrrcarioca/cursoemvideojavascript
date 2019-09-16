e28fdb28c2639bd7f708edc4e0d6d655eacf0249
bhrrcarioca <bhrrcarioca@gmail.com>
2019-09-16 19:24:32 -0300

    novas aulas

---
+++ aula11/ex008.js
@@ -0,0 +1,7 @@
+var vel = 72.5
+console.log(`A velocidade é de ${vel} km/h`)
+if (vel > 60){ // condição simples
+    console.log(`Você ultrapassou a velocidade permitida. MULTADO`)
+}
+
+console.log(`Dirija sempre usando cinto de segurança`)
